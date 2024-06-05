105552c61ff350ab6f52a45de23b1742ff3678e6
shavlenkov <pashashavlenkov@gmail.com>
2024-06-05 16:27:23 +0300

    done task 2 from lesson 1

---
+++ Lesson_1/Task_2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int number_1 = 0;
+    int number_2 = 0;
+
+    cout << "Enter the first number: ";
+    cin >> number_1;
+    cout << "Enter the second number: ";
+    cin >> number_2;
+
+    cout << "Sum: " << number_1 + number_2;
+}
