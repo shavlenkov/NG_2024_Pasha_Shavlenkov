7a2d09c5959ad3762122b07a2b3a4055efc4dc58
shavlenkov <pashashavlenkov@gmail.com>
2024-07-14 14:53:39 +0300

    added complex roots in task 5 from lesson 1

--- Lesson_1/Task_5/main.cpp
+++ Lesson_1/Task_5/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <complex>
 
 using namespace std;
 
@@ -17,16 +18,19 @@ int main() {
 
     double discriminant = pow(number_b, 2) - 4 * number_a * number_c;
 
-    if(discriminant > 0) {
-        double x1 = (-number_b + sqrt(discriminant)) / (2 * number_a);
-        double x2 = (-number_b - sqrt(discriminant)) / (2 * number_a);
+    if (discriminant > 0) {
+        double root_x1 = (-number_b + sqrt(discriminant)) / (2 * number_a);
+        double root_x2 = (-number_b - sqrt(discriminant)) / (2 * number_a);
 
-        if (discriminant == 0) {
-            cout << "The equation has one root: x =" << x1;
-        } else {
-            cout << "The equation has two roots: x1 = " << x1 << ", x2 = " << x2;
-        }
+        cout << "The equation has two roots: x1 = " << root_x1 << ", x2 = " << root_x2;
+    } else if (discriminant == 0) {
+        double root_x = -number_b / (2 * number_a);
+
+        cout << "The equation has one root: x = " << root_x;
     } else {
-        cout << "The equation has no real roots";
+        complex<double> root_x1 = complex<double>(-number_b, sqrt(-discriminant)) / (2 * number_a);
+        complex<double> root_x2 = complex<double>(-number_b, -sqrt(-discriminant)) / (2 * number_a);
+
+        cout << "The equation has two complex roots: x1 = " << root_x1 << ", x2 = " << root_x2;
     }
 }
