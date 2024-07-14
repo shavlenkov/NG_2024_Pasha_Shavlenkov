74a66c2c3c83330aa22fe01baa406b209c3ebea9
shavlenkov <pashashavlenkov@gmail.com>
2024-07-14 14:52:35 +0300

    fixed the conditions in task 3 from lesson 1

--- Lesson_1/Task_3/main.cpp
+++ Lesson_1/Task_3/main.cpp
@@ -8,11 +8,11 @@ int main() {
     cout << "How much do you make?" << endl;
     cin >> salary;
 
-    if(salary < 1001) {
+    if(salary < 1000) {
         cout << "Work more" << endl;
     }
 
-    if(salary > 1000) {
+    if(salary > 999) {
         if(salary < 1000000) {
             cout << "Good job!" << endl;
         }
